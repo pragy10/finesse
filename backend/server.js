2fa4f24fdeed0a691aa973c0a17d91f393e3ed6b
pragy10 <pragya.skr10@gmail.com>
2025-07-14 19:34:32 +0400

    added

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => cb(null,'uploads/'),
+    filename: (req,file,cb) => cb(null,Date.now()+"-"+file.originalname)
+});
+const upload = multer({storage});
+
+app.post('/upload', upload.single('file'), (req,res)=>{
+    res.json({message: 'File uploaded successfully! ', file: req.file});
+});
+
+app.listen(3001, ()=> console.log('Server running on http://localhost:3001'));
 
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import FileUpload from './FileUpload';
+
+function App() {
+  return(
+    <div>
+      <h1>File Upload</h1>
+      <FileUpload/>
+    </div>
+  );
+}
+
+export default App
